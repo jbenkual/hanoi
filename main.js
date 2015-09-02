b871d1e289cadcb68602921ffb14db4fa97299e5
jbenkual <joseph@appthegame.co>
2015-09-02 10:01:51 -0700

    Added support for mobile screens
    The screen will now rotate on small screens using media queries
    The css will scale to screen size on larger screens
    Game functioality is complete (drag and drop between containers)
    A reset button was added for convenience

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var returnSpot;
+var moves = 0;
+
+var lane1 = [1,2,3];
+var lane2 = [];
+var lane3 = [];
+var lanes = { 1: lane1, 2: lane2, 3: lane3};
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -12,6 +18,49 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  console.log("data = " + data);
-  ev.target.appendChild(document.getElementById(data));
+  var loc = ev.target.id[1];
+  if(ev.target.id[0] !== 'c') {
+  	return;
+  }
+  if(ev.target === returnSpot) {
+  	return;
+  }
+  if(validateMove(data, loc)){
+  	var oldLoc = data;
+  	if(typeof(returnSpot) !== 'undefined') {
+  		var oldLoc = returnSpot.id[1];
+  	}
+  	if(!validateMove(data, oldLoc)) {
+  		return;
+  	}
+  	moves++;
+  	var index = lanes[oldLoc].indexOf(data);
+  	lanes[loc].push(data);
+  	lanes[oldLoc].splice(index, 1)
+  	document.getElementById("count").textContent = "Moves: " + moves.toString();
+  	ev.target.appendChild(document.getElementById(data));
+  }
+}
+
+function validateMove(data, loc) {
+	var arr = lanes[loc];
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] > data) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function restart() {
+	moves = 0;
+	document.getElementById("count").textContent = "Moves: " + moves.toString();
+	for (var i =1; i < 4; i++) {
+		var el = document.getElementById(i);
+		console.log(el);
+		document.getElementById("c1").appendChild(el);
+		lanes[1] = [1,2,3];
+		lanes[2] = [];
+		lanes[3] = [];
+	}
 }
